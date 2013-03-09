2b5268e98ca224c8bdd407874a6fde6b574a4ae7
Neckara <Neckara.LastDungeon@gmail.com>
2013-03-09 11:41:55 +0100

    Refactoring ParseException
    Que la personne ayant mis un using std dans le .h se dénonce ! ><

--- src/Handle/parseexception.h
+++ src/Handle/parseexception.h
@@ -1,27 +1,24 @@
 #ifndef PARSEEXCEPTION_H
 #define PARSEEXCEPTION_H
 
-#include "../exception.h"
 #include <string>
 
-using std::string;
-
+#include "../exception.h"
 
 class ParseException : public Exception
 {
 public:
     template<typename T>
-    static ParseException buildParseException(const string &MsgError, EXCEPTIONS_ARGS);
+    static ParseException buildParseException(const std::string &MsgError, EXCEPTIONS_ARGS);
 protected:
     ParseException(EXCEPTIONS_ARGS_CST);
 };
 
 template<typename T>
-ParseException ParseException::buildParseException(const string &MsgError, EXCEPTIONS_ARGS_IMPL)
+ParseException ParseException::buildParseException(const std::string &MsgError, EXCEPTIONS_ARGS_IMPL)
 {
-    string errorMessage = Exception::buildErrorMessage(MsgError,EXCEPTIONS_VALUES);
+    std::string errorMessage = Exception::buildErrorMessage(MsgError,EXCEPTIONS_VALUES);
     return ParseException(errorMessage, MsgError, EXCEPTIONS_VALUES);
-
 }
 
 
