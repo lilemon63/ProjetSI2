a304589cf3db0fdada3b872feec3d7918a9b8f28
Neckara <Neckara.LastDungeon@gmail.com>
2013-03-09 14:55:12 +0100

    Refactoring HandleParameters

--- src/Handle/Parameters/handleparameters.cpp
+++ src/Handle/Parameters/handleparameters.cpp
@@ -1,12 +1,13 @@
-#include "handleparameters.h"
-#include "sourceparameters.h"
-#include "slider.h"
 #include "dial.h"
+#include "slider.h"
 #include "spinbox.h"
-#include "radiobutton.h"
-#include "colorselection.h"
 #include "checkbox.h"
 #include "combobox.h"
+#include "radiobutton.h"
+#include "colorselection.h"
+#include "handleparameters.h"
+#include "sourceparameters.h"
+
 
 HandleParameters::HandleParameters()
     : m_source(nullptr)
@@ -14,11 +15,21 @@ HandleParameters::HandleParameters()
 }
 
 
-void HandleParameters::showParameters(QWidget * parent)
+HandleParameters::~HandleParameters(void)
 {
     if(m_source)
-        m_source->showParameters(parent);
+        m_source->removeSuscriber(this);
 }
+
+/*---------------------------------------------------------------------------------------------------
+------------------------------------------------PUBLIC-----------------------------------------------
+---------------------------------------------------------------------------------------------------*/
+
+void HandleParameters::acceptChanges(QVariant value)
+{
+    QVariant::setValue(value);
+}
+
 
 void HandleParameters::changeSources(SourceParameters *source)
 {
@@ -29,16 +40,11 @@ void HandleParameters::changeSources(SourceParameters *source)
         source->addSuscriber(this);
 }
 
+
 void HandleParameters::hideParameters(void)
 {
     if(m_source)
         m_source->hideParameters();
-}
-
-HandleParameters::~HandleParameters(void)
-{
-    if(m_source)
-        m_source->removeSuscriber(this);
 }
 
 
@@ -47,17 +53,45 @@ void HandleParameters::setActionOnChangeValue(std::function<void(QVariant, Handl
     m_lambda = fct;
 }
 
-void HandleParameters::acceptChanges(QVariant value)
+
+void HandleParameters::showParameters(QWidget * parent)
 {
-    QVariant::setValue(value);
+    if(m_source)
+        m_source->showParameters(parent);
 }
 
-std::shared_ptr<HandleParameters> HandleParameters::build_slider( const QString & label = QString(), int defaultValue, int min, int max)
+/*---------------------------------------------------------------------------------------------------
+------------------------------------------------BUILD------------------------------------------------
+---------------------------------------------------------------------------------------------------*/
+
+std::shared_ptr<HandleParameters> HandleParameters::build_checkbox(const QString & label,
+                                                                   const QStringList & boxes)
 {
     std::shared_ptr<HandleParameters> newParam = std::shared_ptr<HandleParameters>(new HandleParameters() );
-    newParam->changeSources(new Slider(label, defaultValue, min, max));
+    newParam->changeSources(new CheckBox(label, boxes));
     return newParam;
 }
+
+
+std::shared_ptr<HandleParameters> HandleParameters::build_colorselection(const QString & label,
+                                                                         const QColor& c)
+{
+
+    std::shared_ptr<HandleParameters> newParam = std::shared_ptr<HandleParameters>(new HandleParameters() );
+    newParam->changeSources(new ColorSelection(label, c));
+    return newParam;
+}
+
+
+std::shared_ptr<HandleParameters> HandleParameters::build_comboBox(const QString & label,
+                                                                   const QStringList & choices,
+                                                                   const QString & defaultValue )
+{
+    std::shared_ptr<HandleParameters> newParam = std::shared_ptr<HandleParameters>(new HandleParameters() );
+    newParam->changeSources(new ComboBox(label, choices, defaultValue));
+    return newParam;
+}
+
 
 std::shared_ptr<HandleParameters> HandleParameters::build_dial(const QString & label, int defaultAngle)
 {
@@ -66,45 +100,38 @@ std::shared_ptr<HandleParameters> HandleParameters::build_dial(const QString & l
     return newParam;
 }
 
-std::shared_ptr<HandleParameters> HandleParameters::build_spinbox(const QString & label)
-{
-    std::shared_ptr<HandleParameters> newParam = std::shared_ptr<HandleParameters>(new HandleParameters() );
-    newParam->changeSources(new SpinBox(label));
-    return newParam;
-}
 
-std::shared_ptr<HandleParameters> HandleParameters::build_radiobutton(const QString & label, const QStringList & boxes)
-{
-    std::shared_ptr<HandleParameters> newParam = std::shared_ptr<HandleParameters>(new HandleParameters() );
-    newParam->changeSources(new RadioButton(label,boxes));
-    return newParam;
-}
-
-std::shared_ptr<HandleParameters> HandleParameters::build_colorselection(const QString & label, const QColor& c)
-{
-
-    std::shared_ptr<HandleParameters> newParam = std::shared_ptr<HandleParameters>(new HandleParameters() );
-    newParam->changeSources(new ColorSelection(label, c));
-    return newParam;
-}
-
-std::shared_ptr<HandleParameters> HandleParameters::build_inputtext(const QString & label, const QString &defaultValue, InputText::Helper helper)
+std::shared_ptr<HandleParameters> HandleParameters::build_inputtext(const QString & label,
+                                                                    const QString &defaultValue,
+                                                                    InputText::Helper helper)
 {
     std::shared_ptr<HandleParameters> newParam = std::shared_ptr<HandleParameters>(new HandleParameters() );
     newParam->changeSources(new InputText(label, defaultValue, helper));
     return newParam;
 }
 
-std::shared_ptr<HandleParameters> HandleParameters::build_checkbox(const QString & label, const QStringList & boxes)
+
+std::shared_ptr<HandleParameters> HandleParameters::build_radiobutton(const QString & label,
+                                                                      const QStringList & boxes)
 {
     std::shared_ptr<HandleParameters> newParam = std::shared_ptr<HandleParameters>(new HandleParameters() );
-    newParam->changeSources(new CheckBox(label, boxes));
+    newParam->changeSources(new RadioButton(label,boxes));
     return newParam;
 }
 
-std::shared_ptr<HandleParameters> HandleParameters::build_comboBox(const QString & label, const QStringList & choices, const QString & defaultValue )
+
+std::shared_ptr<HandleParameters> HandleParameters::build_slider( const QString & label = QString(),
+                                                                  int defaultValue, int min, int max)
 {
     std::shared_ptr<HandleParameters> newParam = std::shared_ptr<HandleParameters>(new HandleParameters() );
-    newParam->changeSources(new ComboBox(label, choices, defaultValue));
+    newParam->changeSources(new Slider(label, defaultValue, min, max));
+    return newParam;
+}
+
+
+std::shared_ptr<HandleParameters> HandleParameters::build_spinbox(const QString & label)
+{
+    std::shared_ptr<HandleParameters> newParam = std::shared_ptr<HandleParameters>(new HandleParameters() );
+    newParam->changeSources(new SpinBox(label));
     return newParam;
 }
--- src/Handle/Parameters/handleparameters.h
+++ src/Handle/Parameters/handleparameters.h
@@ -1,74 +1,82 @@
 #ifndef HANDLEPARAMETERS_H
 #define HANDLEPARAMETERS_H
 
-#include <QVariant>
-#include <memory>
-#include <iostream>
-#include <functional>
 #include <QColor>
+#include <memory>
+#include <QVariant>
+#include <functional>
 
 #include "inputtext.h"
 
 class SourceParameters;
 
 /** @brief Parameters for VirtualHandle */
-class HandleParameters : public QVariant
+class HandleParameters final : public QVariant
 {
 public:
     /** @brief Create an HandleParameters*/
     explicit HandleParameters(void);
-
+    HandleParameters( const HandleParameters & ) = delete;
+    HandleParameters & operator=( const HandleParameters & ) = delete;
     ~HandleParameters(void);
-
-
-    /** @brief Show the Parameters on the parameters' area
-        @param QWidget * parent : parent */
-    void showParameters(QWidget * parent);
-
-    /** @brief Hide the Parameters on the parameters' area  */
-    void hideParameters(void);
-
-
-    /** @brief Change the source
-        @param SourceParameters * m_source : */
-    void changeSources(SourceParameters * source);
-
-    /** */
-    void setActionOnChangeValue(std::function<void(QVariant, HandleParameters *)> fct);
-
-    template<typename T>
-    void setValue(const T & value)
-    {
-        if( m_lambda )
-            m_lambda( QVariant(value) , this);
-        else
-            QVariant::setValue(value);
-    }
 
     /** /!\ Call it only in lambda giving in setActionOnChangeValue() */
     void acceptChanges(QVariant value);
 
 
-    static std::shared_ptr<HandleParameters> build_slider( const QString & label, int defaultValue = 0, int min = 0, int max = 100);
+    static std::shared_ptr<HandleParameters> build_checkbox(const QString & label,
+                                                            const QStringList & boxes = QStringList() );
+    static std::shared_ptr<HandleParameters> build_colorselection(const QString & label,
+                                                                  const QColor& = QColor(255,0,0));
+    static std::shared_ptr<HandleParameters> build_comboBox(const QString & label,
+                                                            const QStringList & choices = QStringList(),
+                                                            const QString & defaultValue = QString() );
     static std::shared_ptr<HandleParameters> build_dial(const QString & label, int defaultAngle = 0);
+    static std::shared_ptr<HandleParameters> build_inputtext(const QString & label = QString(),
+                                                             const QString &defaultValue = QString(),
+                                                             InputText::Helper helper = InputText::None);
+    static std::shared_ptr<HandleParameters> build_radiobutton(const QString & label,
+                                                               const QStringList & boxes = QStringList() );
+    static std::shared_ptr<HandleParameters> build_slider( const QString & label, int defaultValue = 0,
+                                                           int min = 0, int max = 100);
     static std::shared_ptr<HandleParameters> build_spinbox(const QString & label = QString() );
-    static std::shared_ptr<HandleParameters> build_radiobutton(const QString & label, const QStringList & boxes = QStringList() );
-    static std::shared_ptr<HandleParameters> build_colorselection(const QString & label, const QColor& = QColor(255,0,0));
-    static std::shared_ptr<HandleParameters> build_inputtext(const QString & label = QString(), const QString &defaultValue = QString(), InputText::Helper helper = InputText::None);
-    static std::shared_ptr<HandleParameters> build_checkbox(const QString & label, const QStringList & boxes = QStringList() );
-    static std::shared_ptr<HandleParameters> build_comboBox(const QString & label, const QStringList & choices = QStringList(), const QString & defaultValue = QString() );
+
+    /** @brief Change the source
+        @param SourceParameters * m_source : */
+    void changeSources(SourceParameters * source);
+
+
+    /** @brief Hide the Parameters on the parameters' area  */
+    void hideParameters(void);
+
+
+    /** */
+    void setActionOnChangeValue(std::function<void(QVariant, HandleParameters *)> fct);
+
+    template<typename T>
+    void setValue(const T & value);
+
+    /** @brief Show the Parameters on the parameters' area
+        @param QWidget * parent : parent */
+    void showParameters(QWidget * parent);
+
 private :
-    /** @brief HandleParameters' Source */
-    SourceParameters * m_source;
-
-    /** @brief copy is forbidden */
-    HandleParameters(const HandleParameters &) : QVariant(0) {};
-
-    /** @brief copy is forbidden */
-    const HandleParameters & operator=(const HandleParameters &){ return *this; };
 
     std::function<void(QVariant, HandleParameters *)> m_lambda;
-    
+
+    /** @brief HandleParameters' Source */
+    SourceParameters * m_source;
 };
+
+
+
+template<typename T>
+void HandleParameters::setValue(const T & value)
+{
+    if( m_lambda )
+        m_lambda( QVariant(value) , this);
+    else
+        QVariant::setValue(value);
+}
 
 #endif // HANDLEPARAMETERS_H
