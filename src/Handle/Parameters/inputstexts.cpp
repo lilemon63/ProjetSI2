56ab1fb6d30546f91ba617b6f08b5d9e2652f63d
Neckara <Neckara.LastDungeon@gmail.com>
2013-03-09 15:00:34 +0100

    Refactoring InputsTexts

--- src/Handle/Parameters/inputstexts.cpp
+++ src/Handle/Parameters/inputstexts.cpp
@@ -1,6 +1,10 @@
+#include <QFrame>
+#include <QLineEdit>
+#include <QHBoxLayout>
+
 #include "inputstexts.h"
 #include "handleparameters.h"
-#include <QHBoxLayout>
+
 
 InputsTexts::InputsTexts(const QString & label, unsigned int number, const QStringList &defaultValue) :
     SourceParameters(label),
@@ -10,13 +14,16 @@ InputsTexts::InputsTexts(const QString & label, unsigned int number, const QStri
     m_frame->setLayout( new QHBoxLayout() );
     m_frame->layout()->setContentsMargins(0,0,0,0);
     m_frame->layout()->setSpacing(0);
+
     unsigned int max = defaultValue.size();
     for(unsigned int i = 0; i != number; ++i)
     {
         m_inputText[i] = new QLineEdit();
-        m_frame->layout()->addWidget(m_inputText[i]);    
+        m_frame->layout()->addWidget(m_inputText[i]);
+
         m_values.push_back( m_inputText[i]->text() );
         m_inputText[i]->setMaximumWidth(50);
+
         m_ids[m_inputText[i] ] = i;
         connect(m_inputText[i], SIGNAL(textChanged(QString)), this, SLOT(valueChanged()));
         if( i < max)
@@ -25,24 +32,14 @@ InputsTexts::InputsTexts(const QString & label, unsigned int number, const QStri
 }
 
 
-void InputsTexts::valueChanged(void)
+InputsTexts::~InputsTexts()
 {
-    QObject * obj = QObject::sender();
-    int id = m_ids[(QLineEdit*)obj];
-    m_values[id] = m_inputText[id]->text();
-    for(HandleParameters * hp : m_suscribers )
-        hp->setValue( m_values );
+    delete [] m_inputText;
 }
 
-void InputsTexts::showParameters(QWidget * parent)
-{
-    setParentLayout(parent, m_frame);
-}
-
-void InputsTexts::hideParameters(void)
-{
-    m_frame->hide();
-}
+/*---------------------------------------------------------------------------------------------------
+------------------------------------------------PUBLIC-----------------------------------------------
+---------------------------------------------------------------------------------------------------*/
 
 void InputsTexts::addSuscriber(HandleParameters * target)
 {
@@ -50,13 +47,36 @@ void InputsTexts::addSuscriber(HandleParameters * target)
     target->setValue( m_values );
 }
 
-InputsTexts::~InputsTexts()
-{
-    delete [] m_inputText;
-}
 
 void InputsTexts::changeValue( const QString & newValue, int id )
 {
     if( m_values[id] != newValue)
         m_inputText[id]->setText(newValue);
+}
+
+
+void InputsTexts::showParameters(QWidget * parent)
+{
+    setParentLayout(parent, m_frame);
+}
+
+
+void InputsTexts::hideParameters(void)
+{
+    m_frame->hide();
+}
+
+
+/*---------------------------------------------------------------------------------------------------
+------------------------------------------------PRIVATE SLOT-----------------------------------------
+---------------------------------------------------------------------------------------------------*/
+
+void InputsTexts::valueChanged(void)
+{
+    QObject * obj = QObject::sender();
+    int id = m_ids[(QLineEdit*)obj];
+    m_values[id] = m_inputText[id]->text();
+
+    for(HandleParameters * hp : m_suscribers )
+        hp->setValue( m_values );
 }
--- src/Handle/Parameters/inputstexts.h
+++ src/Handle/Parameters/inputstexts.h
@@ -1,12 +1,14 @@
 #ifndef INPUTSTEXTS_H
 #define INPUTSTEXTS_H
 
-#include <QObject>
-#include <QLineEdit>
-#include <QFrame>
-#include <QStringList>
 #include <map>
+#include <QObject>
+#include <QStringList>
+
 #include "sourceparameters.h"
+
+class QFrame;
+class QLineEdit:
 
 class InputsTexts : public QObject, public SourceParameters
 {
@@ -14,20 +16,22 @@ class InputsTexts : public QObject, public SourceParameters
 public:
     enum Helper{None, Directory, SaveFile, OpenFile};
 
-    explicit InputsTexts(const QString & label = QString(), unsigned int number = 2, const QStringList &defaultValue = QStringList());
-    virtual void showParameters(QWidget * parent);
-    virtual void hideParameters(void);
-    virtual void addSuscriber(HandleParameters * target);
+    explicit InputsTexts(const QString & label = QString(), unsigned int number = 2,
+                         const QStringList &defaultValue = QStringList());
     virtual ~InputsTexts();
+
+    virtual void addSuscriber(HandleParameters * target);
     virtual void changeValue( const QString & newValue, int id );
+    virtual void hideParameters(void);
+    virtual void showParameters(QWidget * parent);
 
 private slots :
     void valueChanged(void);
 private :
+    std::map<QLineEdit *, int> m_ids;
     QLineEdit ** m_inputText;
     QFrame * m_frame;
     QStringList m_values;
-    std::map<QLineEdit *, int> m_ids;
 };
 
 #endif // INPUTSTEXTS_H
