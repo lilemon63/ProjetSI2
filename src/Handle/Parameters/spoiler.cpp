dc261f1d2fd92804480adcf012ce15837748b728
Neckara <Neckara.LastDungeon@gmail.com>
2013-03-09 16:14:52 +0100

    Refactoring Spoiler
    Handle totalement fini, on commande View

--- src/Handle/Parameters/spoiler.cpp
+++ src/Handle/Parameters/spoiler.cpp
@@ -1,19 +1,28 @@
+#include <QLabel>
+#include <QFrame>
+#include <QPushButton>
+#include <QHBoxLayout>
+
 #include "spoiler.h"
-#include <iostream>
+#include "handleparameters.h"
+#include "sourceparameters.h"
 
 Spoiler::Spoiler()
     :
-    m_frame(new QFrame() ),
-    m_button(new QPushButton()),
-    m_button_icon(new QPushButton()),
-    m_layout(new QHBoxLayout(this)),
-    m_title(new QFrame())
+    m_button( new QPushButton() ),
+    m_button_icon( new QPushButton() ),
+    m_frame( new QFrame() ),
+    m_layout( new QHBoxLayout(this) ),
+    m_title( new QFrame() )
 {
     m_button_icon->setIcon(QIcon(QPixmap("low_arrow.png")));
+
     m_layout->addWidget(m_button_icon);
     m_layout->addWidget(m_button);
+
     m_layout->addStretch();
     m_layout->setContentsMargins(0,0,0,0);
+
     m_layout->setMargin(0);
     m_title->setLayout(m_layout);
 
@@ -23,9 +32,19 @@ Spoiler::Spoiler()
     m_button_icon->setFlat(true);
 
     QObject::connect(m_button,SIGNAL(clicked()),this,SLOT(whenClicked()));
-
     QObject::connect(m_button_icon,SIGNAL(clicked()),this,SLOT(whenClicked()));
 }
+
+/*---------------------------------------------------------------------------------------------------
+------------------------------------------------PUBLIC-----------------------------------------------
+---------------------------------------------------------------------------------------------------*/
+
+
+void Spoiler::changeAffName( const QString & affname, Numbering & num)
+{
+    m_button->setText( num.text() + " " + affname );
+}
+
 
 void Spoiler::hideAll()
 {
@@ -34,11 +53,40 @@ void Spoiler::hideAll()
     m_hiden = true;
 }
 
+
+void Spoiler::hideParameters(void)
+{
+    m_frame->hide();
+}
+
+
+void Spoiler::setParam(const VirtualHandle::ListParameters &listParameters,
+                       std::shared_ptr<HandleParameters> viewParam,
+                       const VirtualHandle::ListDependancies &dependancies,
+                       const QString &name, QWidget * parent, Numbering &num )
+{
+    m_button->setText( num.text() + " " + name );
+
+    for(auto param : listParameters)
+        param->showParameters(m_frame );
+    viewParam->showParameters(m_frame );
+
+    for(auto mobile : dependancies)
+        mobile->showParameters( m_frame , num );
+
+    parent->layout()->addWidget(m_title);
+    parent->layout()->addWidget(m_frame);
+    m_title->show();
+    m_frame->hide();
+}
+
+
 void Spoiler::showAll()
 {
     m_title->show();
     m_frame->show();
 }
+
 
 void Spoiler::showParameters(QWidget * parent)
 {
@@ -49,50 +97,28 @@ void Spoiler::showParameters(QWidget * parent)
     m_frame->show();
 }
 
-void Spoiler::hideParameters(void)
-{
-    m_frame->hide();
-}
-
-void Spoiler::setParam(const VirtualHandle::ListParameters &listParameters, std::shared_ptr<HandleParameters> viewParam,
-                       const VirtualHandle::ListDependancies &dependancies,
-                       const QString &name, QWidget * parent, Numbering &num)
-{
-    m_button->setText( num.text() + " " + name );
-    for(auto param : listParameters)
-        param->showParameters(m_frame );
-    viewParam->showParameters(m_frame );
-
-    for(auto mobile : dependancies)
-        mobile->showParameters( m_frame , num );
-    parent->layout()->addWidget(m_title);
-    parent->layout()->addWidget(m_frame);
-    m_title->show();
-    m_frame->hide();
-
-}
-
-void Spoiler::changeAffName( const QString & affname, Numbering & num)
-{
-    m_button->setText( num.text() + " " + affname );
-}
-
-void Spoiler::whenClicked()
-{
-    if(m_hiden){
-        showAll();
-        m_button_icon->setIcon(QIcon(QPixmap("high_arrow.png")));
-    }
-    else{
-        hideParameters();
-        m_button_icon->setIcon(QIcon(QPixmap("low_arrow.png")));
-    }
-    m_hiden = !m_hiden;
-
-}
-
 
 QWidget * Spoiler::widget(void)
 {
     return m_frame;
+}
+
+
+/*---------------------------------------------------------------------------------------------------
+------------------------------------------------PUBLIC SLOT------------------------------------------
+---------------------------------------------------------------------------------------------------*/
+
+void Spoiler::whenClicked()
+{
+    if(m_hiden)
+    {
+        showAll();
+        m_button_icon->setIcon( QIcon( QPixmap("high_arrow.png") ) );
+    }
+    else
+    {
+        hideParameters();
+        m_button_icon->setIcon( QIcon( QPixmap("low_arrow.png") ) );
+    }
+    m_hiden = !m_hiden;
 }
--- src/Handle/Parameters/spoiler.h
+++ src/Handle/Parameters/spoiler.h
@@ -1,24 +1,23 @@
 #ifndef SPOILER_H
 #define SPOILER_H
 
-#include <QObject>
-#include <list>
-#include "handleparameters.h"
-#include "../virtualhandle.h"
-#include <QPushButton>
-#include <QLabel>
-#include <QHBoxLayout>
-#include "sourceparameters.h"
-#include <string>
-#include <QFrame>
+#include <QWidget>
 
+#include "../virtualhandle.h"
+
+class QLabel;
+class QFrame;
 class Numbering;
+class QPushButton;
+class QHBoxLayout;
+class HandleParameters;
+class SourceParameters;
 
 /**
 * @class Spoiler
 * Used to spoil an handler
 */
-class Spoiler : public QWidget
+class Spoiler : public QWidget //final
 {
     Q_OBJECT
 public:
@@ -26,17 +25,16 @@ public:
 * @brief Constructor
 */
     Spoiler();
+    Spoiler( const Spoiler & ) = delete;
+    Spoiler & operator=( const Spoiler & ) = delete;
 
+    void changeAffName( const QString & affname, Numbering & num);
     /**
     * @brief Used to hide all HandlerParameters of a Virtual Handle
     */
     void hideAll();
 
-    virtual void showParameters(QWidget * parent);
-
     virtual void hideParameters(void);
-
-    virtual ~Spoiler(){}
 
     void setParam(const VirtualHandle::ListParameters & listParameters, std::shared_ptr<HandleParameters> viewParam,
                   const VirtualHandle::ListDependancies & dependancies,const QString & name, QWidget * parent,
@@ -46,19 +44,17 @@ public:
     * @param parent : Widget on which they'll be show
     */
     void showAll();
-
-    void changeAffName( const QString & affname, Numbering & num);
+    virtual void showParameters(QWidget * parent);
 
     QWidget * widget(void);
 public slots:
     void whenClicked();
 private:
-    QFrame *m_frame;
     QPushButton *m_button,*m_button_icon;
+    QFrame *m_frame;
+    bool m_hiden = true;
     QHBoxLayout *m_layout;
     QFrame * m_title;
-
-    bool m_hiden = true;
 };
 
 #endif // SPOILER_H
